047bdeafcc49bf481bf352a3f06a0a89095cd3b5
Adrian Unger <dev@staydecent.ca>
2017-05-21 16:44:05 -0700

    Add Button.

--- app/components/pages/home/index.js
+++ app/components/pages/home/index.js
@@ -1,10 +1,12 @@
 import Link from '/components/elements/link'
+import Button from '/components/elements/button'
 
 const Home = ({url}) =>
   <div>
     <h1>Hello World</h1>
     <p>{url}</p>
     <Link to='http://google.ca'>Google</Link>
+    <Button to='http://news.ycombinator.com'>Hacker News</Button>
   </div>
 
 export default Home
