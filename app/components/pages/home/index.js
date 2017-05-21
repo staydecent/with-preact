c587fb0898ad5889b4d7514df2b807932bccd34a
Adrian Unger <dev@staydecent.ca>
2017-05-20 21:04:28 -0700

    Add utils and Link component.

--- app/components/pages/home/index.js
+++ app/components/pages/home/index.js
@@ -1,7 +1,10 @@
+import Link from '/components/elements/link'
+
 const Home = ({url}) =>
   <div>
     <h1>Hello World</h1>
     <p>{url}</p>
+    <Link to='http://google.ca'>Google</Link>
   </div>
 
 export default Home
