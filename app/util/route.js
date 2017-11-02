1501b295f474cc26330a772bd5ebe3bcfa5c347f
Adriaan Mulder <adriaan.mulder@gmail.com>
2017-11-01 19:17:19 -0700

    Enforce that Router children are all Route components.

--- app/util/route.js
+++ app/util/route.js
@@ -1,32 +1,49 @@
 import {equals} from 'ramda'
 import PreactRouter from 'preact-router'
 import {map, reduce} from 'wasmuth'
-import compose from '/util/compose'
+import {compose, setNodeName} from '/util/compose'
 import {set, dispatch, getState} from '/store'
 import {routes} from '/routes'
 
 const {cloneElement} = Preact
 
 /**
- * Use instaed of preact-router Router component.
+ * Use instead of preact-router Router component.
  * All children should be Route components (defined below).
  * - Add key prop to Route children so that when route changes,
  *   the current route component will unmount and the new one will mount.
  * - Add path prop to Route children based on their name props.
  */
-export const Router = ({children, routes, ...props}) =>
-  <PreactRouter {...props}>
-    {map((route, i) => cloneElement(route, {
-      key: i,
-      path: routes[route.attributes.name].path
-    }), children)}
-  </PreactRouter>
+export const Router = ({children, routes, ...props}) => {
+  for (var i = 0; i < children.length; i++) {
+    if (children[i].nodeName.name !== 'Route') {
+      throw new Error('The only children of Router should be Route components')
+    }
+  }
+
+  return (
+    <PreactRouter {...props}>
+      {map((route, i) => cloneElement(route, {
+        key: i,
+        path: routes[route.attributes.name].path
+      }), children)}
+    </PreactRouter>
+  )
+}
 
 /**
  * Add preact-router props into the atom state
+ * Should only be used as child of Router component (above) the Router
+ * coponent will replace the name prop given to this component
+ * with the corresponding path as defined in the routes prop
+ * given to the Router component
+ * Because Router above wraps the preact-router Router component
+ * this component is given the props "matches" and "name" by preact-router
+ * Router. These props are added to the state.
  */
-export const Route = compose({
-  componentWillMount () {
+export const Route = compose(
+  setNodeName('Route'),
+  function componentWillMount () {
     const currentValues = getState().route
     const newValues = {
       args: this.props.matches,
@@ -37,10 +54,10 @@ export const Route = compose({
       dispatch(set('route', newValues))
     }
   },
-  render ({component: Component}) {
+  function render ({component: Component}) {
     return <Component />
   }
-})
+)
 
 /**
  * Get the path string for the route with name `name`
