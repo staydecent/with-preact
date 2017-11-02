66e72889f45e2278aee597e96b0d6e79fcebc550
Adriaan Mulder <adriaan.mulder@gmail.com>
2017-11-01 20:00:19 -0700

    Switch from atom to redux.

--- app/store.js
+++ app/store.js
@@ -1,23 +1,41 @@
+import {createStore} from 'redux'
 import atom from 'atom'
 import {watchStore} from 'wasmuth'
-import {set as lensSet, update as lensUpdate, remove as lensRemove} from 'atom-lens-reducer'
+import {
+  set as lensSet,
+  update as lensUpdate,
+  remove as lensRemove,
+  reducer as lensReducer
+} from 'atom-lens-reducer'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 import dropdownReducer from '/components/elements/dropdown/reducer'
 import linkReducer from '/components/elements/link/reducer'
 import modalReducer from '/components/elements/modal/reducer'
 import formReducer from '/components/elements/form/reducer'
 
+const combine = reducers => (state, action) => {
+  var newState = state || {}
+  for (var i = 0; i < reducers.length; i++) {
+    newState = reducers[i](newState, action)
+  }
+  return newState
+}
+
 const initialState = {
   url: window.location.pathname,
   modals: {}
 }
 
-export const store = atom([
+const reducers = [
   linkReducer,
   dropdownReducer,
   modalReducer,
-  formReducer
-], initialState)
+  formReducer,
+  (state, action) => lensReducer(action, state)
+]
+
+export const store = createStore(combine(reducers), initialState, composeWithDevTools())
 export const dispatch = store.dispatch
 export const getState = store.getState
 export const subscribe = store.subscribe
