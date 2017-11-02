7faf4d8ced1fa62f17178287957e04024fccc9d0
Adrian Unger <dev@staydecent.ca>
2017-11-02 14:48:21 -0700

    Main does not need to subscribe to state changes.

--- app/index.js
+++ app/index.js
@@ -2,31 +2,22 @@ import {getState, subscribe} from '/store'
 import routes from '/routes'
 
 import {Router, Route} from '/util/route'
-import compose from '/util/compose'
 
 import Header from '/components/elements/header'
 import Home from '/components/pages/home'
 import Resource from '/components/pages/resource'
 import Components from '/components/pages/components'
 
-const Main = compose(
-  {state: getState()},
-  function componentWillMount () {
-    subscribe(() => this.setState(getState()))
-  },
-  function render () {
-    console.log('State', getState())
-    return (
-      <div>
-        <Header />
-        <Router routes={routes}>
-          <Route component={Home} name='home' />
-          <Route component={Resource} name='resource' />
-          <Route component={Components} name='components' />
-        </Router>
-      </div>
-    )
-  }
-)
+subscribe(() => console.log('State', getState()))
+
+const Main = () =>
+  <div>
+    <Header />
+    <Router routes={routes}>
+      <Route component={Home} name='home' />
+      <Route component={Resource} name='resource' />
+      <Route component={Components} name='components' />
+    </Router>
+  </div>
 
 Preact.render(<Main />, document.body)
