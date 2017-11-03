00990595581868a719e3bcdbabb4ee138848d119
Adrian Unger <dev@staydecent.ca>
2017-11-03 15:26:54 -0700

    Add back the logic to prevent body scrolling when a modal is open.

--- app/components/elements/modal/index.js
+++ app/components/elements/modal/index.js
@@ -4,6 +4,20 @@ import {some, diffObj} from 'wasmuth'
 import {dispatch, watchPath} from '/store'
 import {closeModal} from './actions'
 
+// Watch for an open modal, if so add a class to body to prevent
+// scrolling behind the modal.
+watchPath(['modals'], (modals, oldModals = {}) => {
+  const hasOpenModal = some((x) => x, modals)
+  if (hasOpenModal) {
+    const diff = Object.keys(diffObj(oldModals, modals))
+    if (diff.length) {
+      document.body.classList.add('modal-open')
+    }
+  } else {
+    document.body.classList.remove('modal-open')
+  }
+})
+
 const Modal = ({
   className = '',
   children
